160207f6f6584ea2dbf35b9024aff51b2fbc1fbd
mapuaptu <mapuaptu@gmail.com>
2018-08-20 00:42:25 +0400

    Добавил кнопку удалить

--- src/Point.js
+++ src/Point.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import removeIcon from './icons/remove.svg';
 
 import styled from 'styled-components';
 
 const StyledPoint = styled.div`
+  position: relative;
   margin-bottom: 20px;
   border: 1px solid #c0c8d0;
   padding: 20px 10px;
   color: #c0c8d0;
+
+  .point__remove {
+    position: absolute;
+    right: 5px;
+    top: 5px;
+    width: 10px;
+    height: 10px;
+    transition: opacity 0.3s linear;
+    cursor: pointer;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+
+    &:hover {
+      opacity: 0.6;
+    }
+  }
 `;
 
 class Point extends Component {
   render() {
     const { title } = this.props;
 
-    return <StyledPoint>{title}</StyledPoint>;
+    return (
+      <StyledPoint className="point">
+        <div className="point__title">{title}</div>
+        <div className="point__remove">
+          <img src={removeIcon} alt="Удалить" />
+        </div>
+      </StyledPoint>
+    );
   }
 }
 
