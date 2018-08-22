6e07c0a9507cf8c33ffca347882171396d58fc54
mapuaptu <mapuaptu@gmail.com>
2018-08-23 02:14:54 +0400

    Поправил ошилку сборки

--- client/src/index.js
+++ client/src/index.js
@@ -5,29 +5,31 @@ import WebFont from 'webfontloader';
 import 'normalize.css';
 import './main.css';
 import registerServiceWorker from './registerServiceWorker';
-import { AppContainer } from 'react-hot-loader';
+// import { AppContainer } from 'react-hot-loader';
 
 WebFont.load({
   google: {
     families: ['Roboto:400', 'sans-serif'],
   },
 });
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <App />
-    </AppContainer>,
-    document.getElementById('root'),
-  );
-};
+// const render = Component => {
+//   ReactDOM.render(
+//     <AppContainer>
+//       <App />
+//     </AppContainer>,
+//     document.getElementById('root'),
+//   );
+// };
+//
+// render(App);
+//
+// if (module.hot) {
+//   module.hot.accept('./App', () => {
+//     render(App);
+//   });
+// }
 
-render(App);
 
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    render(App);
-  });
-}
