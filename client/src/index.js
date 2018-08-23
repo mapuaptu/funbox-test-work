a39a66804502cd57261565aaa84d2c68aec3106d
mapuaptu <mapuaptu@gmail.com>
2018-08-23 20:44:02 +0400

    Правка

--- client/src/index.js
+++ client/src/index.js
@@ -5,31 +5,29 @@ import WebFont from 'webfontloader';
 import 'normalize.css';
 import './main.css';
 import registerServiceWorker from './registerServiceWorker';
-// import { AppContainer } from 'react-hot-loader';
+import { AppContainer } from 'react-hot-loader';
 
 WebFont.load({
   google: {
     families: ['Roboto:400', 'sans-serif'],
   },
 });
-ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
 
-// const render = Component => {
-//   ReactDOM.render(
-//     <AppContainer>
-//       <App />
-//     </AppContainer>,
-//     document.getElementById('root'),
-//   );
-// };
-//
-// render(App);
-//
-// if (module.hot) {
-//   module.hot.accept('./App', () => {
-//     render(App);
-//   });
-// }
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('root'),
+  );
+};
 
+render(App);
 
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
