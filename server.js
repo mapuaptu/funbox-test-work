76c12e4025d8ee53030bf717f3a0def0c338e1c8
mapuaptu <mapuaptu@gmail.com>
2018-09-03 07:05:01 +0400

    Добавил babel для поддержки es6 на сервере

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const app = express();
+import express from "express";
 
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.static(`${__dirname}/client/build`));
